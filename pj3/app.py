19dd161a6c7e3a05630a4ec3937275b9592f84b5
BIBI <dingdongx2@hanyang.ac.kr>
2018-11-09 15:58:06 +0900

    file version) login 기능 추가

--- pj3/app.py
+++ pj3/app.py
@@ -4,20 +4,26 @@ from flask import Flask, render_template, redirect, request
 import psycopg2 as pg
 import psycopg2.extras
 import psycopg2.extensions
+import csv
 
 app = Flask(__name__)
 
-db_connect = pg.connect(
-# conn_str = pg.connect(
-# conn_str = pg_connection.connection(
-    dbname="soyoung",
-    user = "soyoung",
-    host = "127.0.0.1",
-    password = "soyoung"
-)
+# db_connect = pg.connect(
+# # conn_str = pg.connect(
+# # conn_str = pg_connection.connection(
+#     dbname="soyoung",
+#     user = "soyoung",
+#     host = "127.0.0.1",
+#     password = "soyoung"
+# )
 
-# conn_str = "dbname=\"soyoung\", user = \"soyoung\", host = \"127.0.0.1\", password = \"soyoung\""
-conn_str = "dbname=soyoung"
+# conn_str = "dbname=soyoung"
+
+# with open('students.csv','r',encoding='utf-8') as f:
+#     rdr = csv.reader(f)
+#     for line in rdr:
+#         print(line)
+#     print('//')
 
 @app.route("/")
 def index():
@@ -28,32 +34,55 @@ def login():
     sid = request.form.get('sid')
     passwd = request.form.get('passwd')
 
-    conn = pg.connect(conn_str)
-    cur = conn.cursor()
-    sql = f"SELECT sid, password FROM students WHERE sid='{sid}'"
-    print(sql)
+    with open('students.csv','r',encoding='utf-8') as f:
+        rdr = csv.reader(f)
+        # for line in rdr:
+        #     print(line)
+        tmp = "none"
+        for line in rdr:
+            if line[0].startswith(sid) and line[1].startswith(passwd):
+                return redirect(f"/{sid}")
+                tmp = line[1]
+                print("exists")
+        return render_template("error.html",msg="Wrong ID/Password")
+        # tmp = line[1] for line in rdr if line[0]==sid
+        # print(tmp)
 
-    cur.execute(sql) #sql execute
-    rows = cur.fetchall()
-    if(len(rows)!=1):
-        return render_template('error.html',msg="Wrong ID")
+    # conn = pg.connect(conn_str)
+    # cur = conn.cursor()
+    # sql = f"SELECT sid, password FROM students WHERE sid='{sid}'"
+    # print(sql)
 
-    print(rows[0])
-    conn.close()
-    print(f"{sid}, {passwd}")
-    return redirect(f"/{sid}")
+    # cur.execute(sql) #sql execute
+    # rows = cur.fetchall()
+    # if(len(rows)!=1):
+    #     return render_template('error.html',msg="Wrong ID")
+    #
+    # print(rows[0])
+    # conn.close()
+    # print("=======================")
+    # print(f"{sid}, {passwd}")
+    # print("=======================")
+    # return redirect(f"/{sid}")
 
 @app.route("/<sid>")
 def portal(sid):
-    conn = pg.connect(conn_str)
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) #결과물이 딕셔너리 형태로
-    sql = f"select sid, sname, major_id, grade, tutor_id from students where sid='{sid}'"
+    # conn = pg.connect(conn_str)
+    # cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) #결과물이 딕셔너리 형태로
+    # sql = f"select sid, sname, major_id, grade, tutor_id from students where sid='{sid}'"
 
-    print(sql)
-    cur.execute(sql)
-    rows = cur.fetchall()
-    conn.close()
-    return render_template("portal.html", stu_data = rows[0])
+    # print(sql)
+    # cur.execute(sql)
+    # rows = cur.fetchall()
+    # conn.close()
+    with open('students.csv','r',encoding='utf-8') as f:
+        rdr = csv.reader(f)
+        tmp = 0
+        for line in rdr:
+            if line[0].startswith(sid):
+                return render_template("portal.html", stu_data = line)
+    render_template("error.html",msg="error01")
+    # return render_template("portal.html", stu_data = rows[0])
 
 @app.route("/<sid>/credits")
 def credits(sid):
